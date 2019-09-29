872da3e913eba341b641911deb0005c9db424014
Andres <andres_lopez1996@yahoo.com>
2019-09-29 17:57:56 +0000

    DuplicateError

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -62,8 +62,5 @@ class PlacesController < ApplicationController
     params.require(:place).permit(:name, :description, :address)
   end
 
-  def show
-    @place = Place.find(params[:id])
-    @comment = Comment.new
-  end
+  
 end
